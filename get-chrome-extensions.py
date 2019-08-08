4cedc461ee73fbc06279d374a5d7f2471e2e0813
cyberiko <53852347+cyberiko@users.noreply.github.com>
2019-08-08 22:45:00 +0300

    Add files via upload

---
+++ get-chrome-extensions.py
@@ -0,0 +1,68 @@
+ 
+#!/usr/bin/python
+
+import requests
+import re
+import os
+import json
+from threading import Thread
+
+
+def checkExtension(result,folderName,extensionID,index,url,headers):
+	r = requests.get(url+extensionID, headers=headers)
+
+	if r.status_code == 200:
+		x=re.search("title\" content=\"(.*?)>", r.content)
+		name=x.group(0).split("\"")[2]
+		result[index]={"folder": folderName,"extension": extensionID, 'name': name}
+		#print(folderName + ":  " + extensionID + ":  " + name)
+	elif r.status_code == 404 and extensionID not in excludes:
+		result[index]={"folder": folderName,"extension": extensionID, 'name': "UNKNOWN"}
+		#print(folderName + ":  " + extensionID + ":  " + "UNKNOWN")
+	else:
+		result[index]={"folder": folderName,"extension": extensionID, 'name': "ERROR"}
+
+
+
+### MAIN ###
+
+excludes = {"pkedcjkdefgpdelpbcmbmeomcjbeemfm","nmmhkkegccagdldgiimedpiccmgmieda", ".DS_Store", "Temp"}
+
+jsonData=""
+
+url="https://chrome.google.com/webstore/detail/"
+headers = {"accept-language": "en-US,en;q=0.9"}
+users=os.listdir("/home")
+users.append('root')
+for i in users:
+	if i=='root':
+		if os.path.isdir("/root/.config/chromium"):
+			baseDir="/root/.config/chromium"
+		else:
+			continue
+	else:
+		baseDir="/home/%s/.config/chromium" % (i)
+	if os.path.isdir("%s" % (baseDir)):
+		with open('%s/Local State' % (baseDir)) as json_file:
+			jsonData=json.load(json_file)
+		folders=os.listdir("%s" % (baseDir))
+		for f in folders:
+			if (f.startswith('Profile ') or f=="Default"):
+				threads = []
+				username=jsonData['profile']['info_cache'][f]['name']
+				extensionsID=os.listdir("%s/%s/Extensions" % (baseDir,f))
+				for e in excludes:
+					if e in extensionsID:
+						extensionsID.remove(e)
+				result = {}
+				print("\nPrinting extensions for user: " + i + "\\" + username)
+				for x in range(len(extensionsID)):
+					process = Thread(target=checkExtension, args=[result,f, extensionsID[x], x, url, headers])
+					process.start()
+					threads.append(process)
+				for process in threads:
+					process.join()
+
+				
+				for n in range(len(result)):
+					print (result[n]['folder'] + ":  " + result[n]['extension'] + ":  " + result[n]['name'])
