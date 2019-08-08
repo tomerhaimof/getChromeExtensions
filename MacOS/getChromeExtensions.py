519982c6ccd566155ddb9699c6458d1776c3c40e
cyberiko <53852347+cyberiko@users.noreply.github.com>
2019-08-08 12:37:31 +0300

    Update getChromeExtensions.py

--- MacOS/getChromeExtensions.py
+++ MacOS/getChromeExtensions.py
@@ -4,37 +4,62 @@ import requests
 import re
 import os
 import json
+from threading import Thread
 
-def checkExtension(folderName,extensionID,url,headers):
+
+excludes = ["pkedcjkdefgpdelpbcmbmeomcjbeemfm","nmmhkkegccagdldgiimedpiccmgmieda",".DS_Store", "Temp"]
+
+
+def checkExtension(result,folderName,extensionID,index,url,headers):
 	r = requests.get(url+extensionID, headers=headers)
+
 	if r.status_code == 200:
 		x=re.search("title\" content=\"(.*?)>", r.content)
 		name=x.group(0).split("\"")[2]
-		print(folderName + ":  " + extensionID + ":  " + name)
+		result[index]={"folder": folderName,"extension": extensionID, 'name': name}
+		#print(folderName + ":  " + extensionID + ":  " + name)
 	elif r.status_code == 404 and extensionID not in excludes:
-		print(folderName + ":  " + extensionID + ":  " + "UNKNOWN")
-
+		result[index]={"folder": folderName,"extension": extensionID, 'name': "UNKNOWN"}
+		#print(folderName + ":  " + extensionID + ":  " + "UNKNOWN")
+	else:
+		result[index]={"folder": folderName,"extension": extensionID, 'name': "ERROR"}
 
 
 
 ### MAIN ###
 
+excludes = {"pkedcjkdefgpdelpbcmbmeomcjbeemfm","nmmhkkegccagdldgiimedpiccmgmieda", ".DS_Store"}
 
+jsonData=""
 
-excludes = ["pkedcjkdefgpdelpbcmbmeomcjbeemfm","nmmhkkegccagdldgiimedpiccmgmieda",".DS_Store", "Temp"]
 url="https://chrome.google.com/webstore/detail/"
 headers = {"accept-language": "en-US,en;q=0.9"}
 users=os.listdir("/Users")
 for i in users:
 	if os.path.isdir("/Users/%s/Library/Application Support/Google/Chrome/" % (i)):
-		folders1=os.listdir("/Users/%s/Library/Application Support/Google/Chrome/" % (i))
-		for f in folders1:
-			if f.startswith('Profile ') or f=="Default":
-				with open('/Users/%s/Library/Application Support/Google/Chrome/Local State' % (i)) as json_file:
-					obj=json.load(json_file)
-				username=obj['profile']['info_cache'][f]['name']
-				extensions=os.listdir("/Users/%s/Library/Application Support/Google/Chrome/%s/Extensions" % (i,f))
-				print("Printing extensions for user: " + i + "/" + username)
-				for e in extensions:
-					checkExtension(f,e,url,headers)
+		with open('/Users/%s/Library/Application Support/Google/Chrome/Local State' % (i)) as json_file:
+			jsonData=json.load(json_file)
+		folders=os.listdir("/Users/%s/Library/Application Support/Google/Chrome/" % (i))
+		for f in folders:
+			if (f.startswith('Profile ') or f=="Default"):
+				threads = []
+				username=jsonData['profile']['info_cache'][f]['name']
+				extensionsID=os.listdir("/Users/%s/Library/Application Support/Google/Chrome/%s/Extensions" % (i,f))
+				for e in excludes:
+					if e in extensionsID:
+						extensionsID.remove(e)
+				result = {}
+				print("\nPrinting extensions for user: " + i + "\\" + username)
+				for x in range(len(extensionsID)):
+					process = Thread(target=checkExtension, args=[result,f, extensionsID[x], x, url, headers])
+					process.start()
+					threads.append(process)
+				for process in threads:
+					process.join()
+
+				
+				for n in range(len(result)):
+					print (result[n]['folder'] + ":  " + result[n]['extension'] + ":  " + result[n]['name'])
+						
 	
+
