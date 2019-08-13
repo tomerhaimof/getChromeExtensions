0c1cab144690736b254043922db832230eb6ad5e
cyberiko <53852347+cyberiko@users.noreply.github.com>
2019-08-13 22:41:41 +0300

    adding chromium paths + bold to headlines

--- Linux/getChromeExtensions.py
+++ Linux/getChromeExtensions.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
 '''
-This script will print all of the Google Xhrome extensions from all users
+This script will print all of the Google Chrome and Chromium extensions from all users
 Written by Tomer Haimof
 '''
+
 import requests
 import re
 import os
@@ -29,7 +30,7 @@ def checkExtension(result,folderName,extensionID,index,url,headers):
 
 ### MAIN ###
 
-excludes = {"pkedcjkdefgpdelpbcmbmeomcjbeemfm","nmmhkkegccagdldgiimedpiccmgmieda", ".DS_Store", "Temp"}
+excludes = ["pkedcjkdefgpdelpbcmbmeomcjbeemfm","nmmhkkegccagdldgiimedpiccmgmieda", ".DS_Store", "Temp"]
 
 jsonData=""
 
@@ -37,35 +38,47 @@ url="https://chrome.google.com/webstore/detail/"
 headers = {"accept-language": "en-US,en;q=0.9"}
 users=os.listdir("/home")
 users.append('root')
+bold='\033[1m'
+resetBold='\033[0m'
+
 for i in users:
+	baseDirs=[]
 	if i=='root':
-		if os.path.isdir("/root/.config/chromium"):
-			baseDir="/root/.config/chromium"
-		else:
+		if os.path.isdir("/root/.config/chromium/Extensions"):
+			baseDirs.append("/root/.config/chromium")
+		if os.path.isdir("/root/.config/google-chrome/Extensions"):
+			baseDirs.append("/root/.config/chromium")
+		if len(baseDirs) == 0:
 			continue
 	else:
-		baseDir="/home/%s/.config/chromium" % (i)
-	if os.path.isdir("%s" % (baseDir)):
-		with open('%s/Local State' % (baseDir)) as json_file:
-			jsonData=json.load(json_file)
-		folders=os.listdir("%s" % (baseDir))
-		for f in folders:
-			if (f.startswith('Profile ') or f=="Default"):
-				threads = []
-				username=jsonData['profile']['info_cache'][f]['name']
-				extensionsID=os.listdir("%s/%s/Extensions" % (baseDir,f))
-				for e in excludes:
-					if e in extensionsID:
-						extensionsID.remove(e)
-				result = {}
-				print("\nPrinting extensions for user: " + i + "\\" + username)
-				for x in range(len(extensionsID)):
-					process = Thread(target=checkExtension, args=[result,f, extensionsID[x], x, url, headers])
-					process.start()
-					threads.append(process)
-				for process in threads:
-					process.join()
+		if os.path.isdir("/home/%s/.config/chromium" % (i)):
+			baseDirs.append("/home/%s/.config/chromium" % (i))
+		if os.path.isdir("/home/%s/.config/google-chrome" % (i)):
+			baseDirs.append("/home/%s/.config/google-chrome" % (i))
+		if len(baseDirs) != 0:
+			for baseDir in baseDirs:
+				baseDirSplit = baseDir.split("/")
+				browser = baseDirSplit[len(baseDirSplit)-1]
+				with open('%s/Local State' % (baseDir)) as json_file:
+					jsonData=json.load(json_file)
+				folders=os.listdir("%s" % (baseDir))
+				for f in folders:
+					if (f.startswith('Profile ') or f=="Default"):
+						threads = []
+						username=jsonData['profile']['info_cache'][f]['name']
+						extensionsID=os.listdir("%s/%s/Extensions" % (baseDir,f))
+						for e in excludes:
+							if e in extensionsID:
+								extensionsID.remove(e)
+						result = {}
+						print(bold + "\nPrinting extensions for user: " + i + "\\" + username + " (browser: " + browser +")" + resetBold)
+						for x in range(len(extensionsID)):
+							process = Thread(target=checkExtension, args=[result,f, extensionsID[x], x, url, headers])
+							process.start()
+							threads.append(process)
+						for process in threads:
+							process.join()
 
-				
-				for n in range(len(result)):
-					print (result[n]['folder'] + ":  " + result[n]['extension'] + ":  " + result[n]['name'])
+						
+						for n in range(len(result)):
+							print ("\t" +result[n]['folder'] + ":  " + result[n]['extension'] + ":  " + result[n]['name'])
